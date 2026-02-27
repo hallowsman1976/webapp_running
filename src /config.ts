9fdbf23baa4e380bc42757ea248de439fc861cbd
hallowsman1976 <hallowsman@gmail.com>
2026-02-27 22:07:51 +0700

    Update config.ts

--- src /config.ts
+++ src /config.ts
@@ -1,4 +1,4 @@
 export const CONFIG = {
   LIFF_ID: "YOUR_LIFF_ID", 
-  GAS_WEBAPP_URL: "YOUR_GAS_WEBAPP_URL", 
+  GAS_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbz9WUhaZfo8BYVUI-mcCAANor22IhVrd0iu1HPkKVRZlXMn8XuvkY9ufki1ha5pfklh/exec", 
 };
